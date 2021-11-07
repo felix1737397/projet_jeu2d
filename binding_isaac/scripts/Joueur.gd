09b0a12f8f02671cd3129c61fac6438c0c8b8e2d
Maxime Boucher <2041805@carrefour.cegepvicto.ca>
2021-11-07 10:22:32 -0500

    Création du joueur, de sont déplacement et de c'est animation

---
+++ binding_isaac/scripts/Joueur.gd
@@ -0,0 +1,65 @@
+extends KinematicBody2D
+
+export(int) var speed = 300
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	Mouvement(velocity)
+	Animation_sprite()
+
+
+func Animation_sprite():
+	if Input.is_action_pressed("A") and !Input.is_action_pressed("S") and !Input.is_action_pressed("W"):
+		$Isaak.play("Gauche")
+
+	elif Input.is_action_pressed("D") and !Input.is_action_pressed("S") and !Input.is_action_pressed("W"):
+		$Isaak.play("Droite")
+
+
+	if Input.is_action_pressed("W"):
+		if Input.is_action_pressed("A"):
+			$Isaak.play("Gauche")
+		elif Input.is_action_pressed("D"):
+			$Isaak.play("Droite")
+		else:
+			$Isaak.play("Haut")
+
+
+	if Input.is_action_pressed("S"):
+		if Input.is_action_pressed("A"):
+			$Isaak.play("Gauche")
+		elif Input.is_action_pressed("D"):
+			$Isaak.play("Droite")
+		else:
+			$Isaak.play("Bas")
+
+
+	if Input.is_action_just_released("W"):
+		$Isaak.play("Inactif")
+
+	if Input.is_action_just_released("S"):
+		$Isaak.play("Inactif")
+
+	if Input.is_action_just_released("A"):
+		$Isaak.play("Inactif")
+
+	if Input.is_action_just_released("D"):
+		$Isaak.play("Inactif")
+
+
+func Mouvement(velocity):
+	if Input.is_action_pressed("W"):
+		velocity.y -= 1
+	if Input.is_action_pressed("S"):
+		velocity.y += 1
+	if Input.is_action_pressed("A"):
+		velocity.x -= 1
+	if Input.is_action_pressed("D"):
+		velocity.x += 1
+	
+	#Pour ne pas que le personnage bouge plus vite en diagonal que normalement
+	velocity = velocity.normalized()
+	
+	move_and_slide(velocity * speed)

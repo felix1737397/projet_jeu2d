a8cbb24e992ac84ccd8716cfc0803ba34ba7911b
Vincent <2039855@carrefour.cegepvicto.ca>
2021-11-20 17:41:37 -0500

    Ajout de scenes pour le tutoriel, ainsi que ses ressources

---
+++ binding_isaac/scenes/action_item.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var changer_scene
+
+func _ready():   
+	connect("body_entered" , self, "_area_entered")     
+	connect("body_exited" , self, "_area_exited")     
+ 
+
+func _process(delta):
+	if changer_scene == true:
+		get_tree().change_scene("res://scene/Tutoriel_item.tscn")
+
+
+func _area_entered(object):  
+	changer_scene = true
+
+
+func _area_exited(object):    
+	changer_scene = false

6269e8ff8ea60939bf68eab0055d1d5135d852b3
Vincent <2039855@carrefour.cegepvicto.ca>
2021-11-23 13:32:13 -0500

    Modif hud

--- binding_isaac/scripts/Map.gd
+++ binding_isaac/scripts/Map.gd
@@ -10,7 +10,20 @@ func _physics_process(delta):
 	pause()
 	
 	$Hud/Texte_argent.text = str(VariableGlobales.joueur_argent)
-				
+	
+	if VariableGlobales.joueur_vie == 2:
+		$Hud/Vie2.show()
+		
+	else:
+		$Hud/Vie2.hide()
+		
+	if VariableGlobales.joueur_vie <= 0:
+		$Hud/Vie.hide()
+		
+	
+	
+	
+			
 func pause():
 	
 
